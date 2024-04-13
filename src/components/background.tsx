4d95ad3fab4f34a75df90853aa0a7231228ce893
JoséRivas <josemiguelrivas1993u@gmail.com>
2024-04-13 18:07:07 -0300

    arreglando cosas

--- src/components/background.tsx
+++ src/components/background.tsx
@@ -6,7 +6,7 @@ export default function Background() {
         <div className='absolute bottom-0 right-[-20%] top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]'></div>
       </div> */}
 
-      <div className='absolute left-0 top-0 z-[-2] h-screen w-full bg-[#0a0a0a] bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]'></div>
+      <div className='absolute left-0 top-0 z-[-2] h-screen w-full bg-gradient-to-br from-[#0d1148] to-[#084c61]'></div>
     </>
   );
 }
